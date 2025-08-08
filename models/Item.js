5a70efd8d5460361e4848b671a461dfcaa4d33e2
Sathish-2399 <2312099@nec.edu.in>
2025-08-08 15:12:24 +0530

    Changes some takes

--- models/Item.js
+++ models/Item.js
@@ -4,3 +4,5 @@ const ItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     quantity: { type: Number, required: true }
 });
+
+module.exports = mongoose.model('Item', ItemSchema);
